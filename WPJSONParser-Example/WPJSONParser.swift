4151e3f640e677758aa379a25f26f255439794fc
Vavelin Kevin <vavelinkevin@gmail.com>
2014-08-08 18:57:24 +0400

    Initial setup

---
+++ WPJSONParser-Example/WPJSONParser.swift
@@ -0,0 +1,13 @@
+//
+//  WPJSONParser.swift
+//  WPJSONParser-Example
+//
+//  Created by Vavelin Kevin on 08/08/14.
+//  Copyright (c) 2014 Vavelin Kevin. All rights reserved.
+//
+
+import UIKit
+
+class WPJSONParser: NSObject {
+   
+}
